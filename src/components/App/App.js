eaefc4c11a6d9b1290062eaeb4256e968fcfcba8
Vivek Ratnavel Subramanian <vivekratnavel90@gmail.com>
2022-07-13 15:59:23 -0700

    Update css

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -69,6 +69,7 @@ class App extends Component {
     const getClientAvatar = `https://robohash.org/${getClientID}`;
 
     const slackChatProps = {
+      closeChatButton: true,
       botName: getClientID,
       channels: [
         {
